00faec812d10a0e3c2bff9f51375c63bf8d6a3e7
robert <rob@collaborizm.com>
2019-04-17 18:32:46 -0400

    schema update

--- src/schema.ts
+++ src/schema.ts
@@ -4,7 +4,7 @@ import YouTubeAPI from 'gapi-to-graphql/google_apis/youtube-v3'
 import { makeExecutableSchema, mergeSchemas, IResolvers } from 'graphql-tools'
 import { PubSub } from 'graphql-subscriptions'
 import { TContext } from '../global'
-import { pg } from './knex'
+import { pg, upsert } from './knex'
 
 export const pubsub = new PubSub()
 
@@ -48,20 +48,44 @@ const handSchema = gql`
 
 const resolvers = {
   Mutation: {
-    authenticate(_, __, ctx: TContext) {
-      console.log('USER', ctx.user)
-      return 'new user'
+    async authenticate(_, __, ctx: TContext): Promise<string> {
+
+      const {
+        sub: google_id,
+        email,
+        email_verified,
+        picture,
+        given_name: first_name,
+        family_name: last_name,
+        locale
+      } = ctx.user
+
+
+      const upsertResponse = await upsert({
+        table: 'users', object: {
+          google_id,
+          email,
+          email_verified,
+          picture,
+          first_name,
+          last_name,
+          locale
+        }, key: 'google_id'
+      })
+
+      return upsertResponse.id
     },
     videoPush(_, { input: { channel, videoId } }: GQL.IVideoPushOnMutationArguments, ctx: TContext) {
 
-
       console.log('video push', ctx)
-      //    videos.push({ videoId, channel })
+
+
+      //  videos.push({ videoId, channel })
       //  pubsub.publish('VIDEO_ADDED', { videoPushed: { id: videoId } })
-      //return videoId
+      //  return videoId
       return ''
     },
-    channelCreate(_, { input: { channelName } }, ctx: TContext) {
+    channelCreate(_, { input: { channelName } }: GQL.IChannelCreateOnMutationArguments, ctx: TContext) {
 
       console.log('creating channel', channelName)
     }
