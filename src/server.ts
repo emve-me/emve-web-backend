ebc2e01e7e5f4192e6de855b2f8c092138d9683d
Robert Lancer <rob@collaborizm.com>
2019-05-01 01:18:12 -0400

    removed some editor config

--- src/server.ts
+++ src/server.ts
@@ -20,12 +20,10 @@ const server = new ApolloServer({
     : {
         settings: {
           'general.betaUpdates': false,
-          'editor.cursorShape': 'line',
           'editor.fontSize': 14,
           'editor.fontFamily': "'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace",
           'editor.theme': 'light',
           'editor.reuseHeaders': true,
-          'prettier.printWidth': 80,
           'request.credentials': 'omit',
           'tracing.hideTracingResponse': true
         }
